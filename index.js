7a3818e047b43efe4c2b8102495c7f7fd97aa91a
unknown <patricio.mpb@gmail.com>
2025-07-08 15:19:18 -0300

    Actualización de los ENDPOINTS getAll y getId

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import environments from "./src/api/config/environments.js";
 import connection from "./src/api/database/db.js";
+import cors from "cors";
 
 const PORT = environments.port;
 const app = express();
@@ -10,11 +11,11 @@ app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hola mundo");
-})
+});
 
-app.get("/autos", async (req,res) =>{
+app.get("/vehiculos", async (req,res) => {
     try{
-        let sql = `SELECT * FROM autos`;
+        let sql = `SELECT * FROM vehiculos`;
         //Al usar [rows] la desestructuración extrae directamente las filas (que es el primer elemento del resultado de la consulta), nos sirve porque hace el codigo mas legible y explicito.
         const [rows] = await connection.query(sql)
     
@@ -22,7 +23,7 @@ app.get("/autos", async (req,res) =>{
             payload: rows,
             message: rows.length === 0 ? "No se encontraron usuarios" : "Usuarios encontrados"
         })
-    }catch(error){
+    } catch(error){
         console.error("Error obteniendo productos", error)
         res.status(500).json({
             error: "Error interno del servidor al obtener productos"
@@ -30,30 +31,13 @@ app.get("/autos", async (req,res) =>{
     }
 })
 
-app.get("/motos", async (req,res) =>{
-    try{
-        let sql = `SELECT * FROM motos`;
-        //Al usar [rows] la desestructuración extrae directamente las filas (que es el primer elemento del resultado de la consulta), nos sirve porque hace el codigo mas legible y explicito.
-        const [rows] = await connection.query(sql)
-    
-        res.status(200).json({
-            payload: rows,
-            message: rows.length === 0 ? "No se encontraron usuarios" : "Usuarios encontrados"
-        })
-    }catch(error){
-        console.error("Error obteniendo productos", error)
-        res.status(500).json({
-            error: "Error interno del servidor al obtener productos"
-        })
-    }
-})
 
-app.get("/autos/:id", async(req, res) => {
+app.get("/vehiculos/:id", async(req, res) => {
     //let id = req.params.id
     try{
         let { id } = req.params;
 
-    let sql = `SELECT * FROM autos where id = ?`;
+    let sql = `SELECT * FROM vehiculos where id = ?`;
 
     let [rows] = await connection.query(sql, [id]);
 
@@ -71,39 +55,12 @@ app.get("/autos/:id", async(req, res) => {
         console.error(`Error obteniendo producto con id ${id}`, error.message);
 
         res.status(500).json({
-            error: "Error intero al obtener producto por id"
+            error: "Error interno al obtener un producto por id"
         })
     }
 })
 
-app.get("/motos/:id", async(req, res) => {
-    //let id = req.params.id
-    try{
-        let { id } = req.params;
 
-    let sql = `SELECT * FROM motos where id = ?`;
-
-    let [rows] = await connection.query(sql, [id]);
-
-    if(rows.length === 0){
-        return res.status(404).json({
-            error: `No se encontro el producto con id ${id}`
-        })
-    }
-
-    res.status(200).json({
-        payload:rows
-    });
-    }
-    catch(error){
-        console.error(`Error obteniendo producto con id ${id}`, error.message);
-
-        res.status(500).json({
-            error: "Error intero al obtener producto por id"
-        })
-    }
-})
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
 })
