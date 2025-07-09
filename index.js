7377d6abd5f5d47f3b940c165857d9ab24773eb5
unknown <alderetelautaro54@gmail.com>
2025-07-09 02:41:33 -0300

    Put terminado

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import environments from "./src/api/config/environments.js";
 import connection from "./src/api/database/db.js";
 import cors from "cors";
+import { parse } from "dotenv";
 
 const PORT = environments.port;
 const app = express();
@@ -9,6 +10,30 @@ const app = express();
 //Middlewares
 app.use(express.json()); //Esto lo que hace es parsear el JSON del body
 app.use(cors());
+
+//Middleware Logger para analizar y registrar las solicitudes
+app.use((req, res, next) => {
+    console.log(`[${new Date().toLocaleDateString()}]${req.method} ${req.url}`);
+    next();
+})
+
+//Middleware de ruta -> Aplicados a rutas especificas y ejecutadas solo cuando una solicitud coincide con la ruta definida
+const validateId = (req, res, next) => {
+    //const id = req.params.id;
+    const { id } = req.params;
+
+    if(!id || isNaN(id)){
+        return res.status(400).json({
+            error: "El id debe ser un número"
+        })
+    }
+    //Convertimos el parametro id (originalmente un string porque viene de la url)
+    req.id = parseInt(id, 10);
+
+    next();
+}
+
+
 
 app.get("/", (req, res) => {
     res.send("Hola mundo");
@@ -33,7 +58,7 @@ app.get("/vehiculos", async (req,res) => {
 })
 
 
-app.get("/vehiculos/:id", async(req, res) => {
+app.get("/vehiculos/:id", validateId, async(req, res) => {
     //let id = req.params.id
     try{
         let { id } = req.params;
@@ -90,7 +115,50 @@ app.post("/vehiculos", async(req, res) => {
     }
 })
 
+app.put("/vehiculos", async(req, res) =>{
+    try{
+        let { id, category, image, name, model, price, km} = req.body;
 
+        if(!id || !category || !image || !name || !model || !price || !km) {
+            return res.status(400).json({
+                message: "Faltan campos requeridos"
+            });
+        }
+
+        // let sql = `
+        //     UPDATE vehiculos
+        //     SET tipo = ?, image = ?, name = ?, model = ?, price = ?, km = ?
+        //     WHERE id = ?
+        // `;
+
+        let sql = `
+            UPDATE vehiculos
+            SET tipo = ?, img = ?, nombre = ?, modelo = ?, precio = ?, km = ?
+            WHERE id = ?
+        `;
+        
+        let [result] = await connection.query(sql, [category, image, name, model, price, km, id])
+
+        //Testeamos que se actualizara.
+        if(result.affectedRows === 0){
+            return res.status(400).json({
+                message: "No se actualizó el vehiculo."
+            })
+        }
+
+        res.status(200).json({
+            message: "Vehiculo actualizado correctamente."
+        })
+
+    }catch(error){
+        console.error("Error al actualizar el producto", error)
+
+        res.status(400).json({
+            message: "Error interno del servidor.",
+            error: error.message
+        })
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
